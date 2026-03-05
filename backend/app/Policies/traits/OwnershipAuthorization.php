5c9b5c0970b385e5d71e4574d58ee835b3b58992
molhot <sakata19991214@gmail.com>
2026-03-05 21:52:10 +0900

    traitの追加ができておらず修正

---
+++ backend/app/Policies/traits/OwnershipAuthorization.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies\Traits;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+trait OwnershipAuthorization
+{
+    use HandlesAuthorization;
+
+    public function before(User $user)
+    {
+        if ($user->is_admin) {
+            return true;
+        }
+    }
+
+    protected function isOwner(User $user, $model): bool
+    {
+        return $user->id === $model->user_id;
+    }
+}
