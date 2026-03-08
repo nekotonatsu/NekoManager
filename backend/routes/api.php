51cc4808f8b0257107bf5ef7be056178a2486079
molhot <sakata19991214@gmail.com>
2026-03-08 17:26:07 +0900

    二重スラッシュを回避

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,12 +7,12 @@ use App\Http\Controllers\Expense\ExpenseController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me', [AuthController::class, 'me']);
     });
 });
 
